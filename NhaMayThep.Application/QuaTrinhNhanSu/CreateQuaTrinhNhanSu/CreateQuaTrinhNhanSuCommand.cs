aeaffd9ea4485c95bf170dd789ad0474cde9e6f0
NemissU <namphse171651@fpt.edu.vn>
2024-01-15 10:19:51 +0700

    Nam:Save before pull origin main

---
+++ NhaMayThep.Application/QuaTrinhNhanSu/CreateQuaTrinhNhanSu/CreateQuaTrinhNhanSuCommand.cs
@@ -0,0 +1,33 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace NhaMayThep.Application.QuaTrinhNhanSu.CreateQuaTrinhNhanSu
+{
+    public class CreateQuaTrinhNhanSuCommand : IRequest<QuaTrinhNhanSuDto>, ICommand
+    {
+        public CreateQuaTrinhNhanSuCommand(string maSoNhanVien
+            , int LoaiQuaTrinhID
+            , DateTime ngayBatDau
+            , DateTime? ngayKetThuc
+            , int phongBanID
+            , int chucVuId
+            , int chucDanhID
+            , string? ghiChu)
+        {
+            
+        }
+        public string MaSoNhanVien { get; set; }
+        public int LoaiQuaTrinhID { get; set; }
+        public DateTime NgayBatDau { get; set; }
+        public DateTime? NgayKetThuc { get; set; }
+        public int PhongBanID { get; set; }
+        public int ChucVuID { get; set; }
+        public int ChucDanhID { get; set; }
+        public string? GhiChu { get; set; }
+    }
+}
