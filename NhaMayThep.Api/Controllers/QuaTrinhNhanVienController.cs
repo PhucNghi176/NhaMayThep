0710261dba9449ad744028f1da8f6e6e03aaa101
NemissU <namphse171651@fpt.edu.vn>
2024-01-15 22:13:38 +0700

    Nam: Done Get, Update QuaTrinhNhanSu

--- NhaMayThep.Api/Controllers/QuaTrinhNhanVienController.cs
+++ NhaMayThep.Api/Controllers/QuaTrinhNhanVienController.cs
@@ -1,8 +1,13 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Mvc;
+using NhaMayThep.Application.PhongBan.GetSinglePhongBan;
+using NhaMayThep.Application.PhongBan;
 using NhaMayThep.Application.QuaTrinhNhanSu;
 using NhaMayThep.Application.QuaTrinhNhanSu.CreateQuaTrinhNhanSu;
 using System.Net.Mime;
+using NhaMayThep.Application.QuaTrinhNhanSu.GetSingleQuaTrinhNhanSu;
+using NhaMayThep.Application.PhongBan.UpdatePhongBan;
+using NhaMayThep.Application.QuaTrinhNhanSu.UpdateQuaTrinhNhanSu;
 
 namespace NhaMayThep.Api.Controllers
 {
@@ -30,5 +35,46 @@ namespace NhaMayThep.Api.Controllers
             var result = await _mediator.Send(command, cancellationToken);            
             return Ok(result);
         }
+
+        [HttpGet("Get-by-ID/{id}")]
+        [ProducesResponseType(typeof(QuaTrinhNhanSuDto), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<QuaTrinhNhanSuDto>> GetByID(
+            [FromRoute] string id,
+            CancellationToken cancellationToken = default)
+        {
+            var result = await _mediator.Send(new GetQuaTrinhNhanSuQuery(id : id), cancellationToken);
+            return result == null ? NotFound() : Ok(result);
+        }
+
+        [HttpPut("Update/{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> Update(
+            [FromRoute] string id,
+            [FromBody] UpdateQuaTrinhNhanSuCommand command,
+            CancellationToken cancellationToken = default)
+        {
+            if (command.ID == default)
+            {
+                command.ID = id;
+            }
+            if (id != command.ID)
+            {
+                return BadRequest();
+            }
+
+            await _mediator.Send(command, cancellationToken);
+            return NoContent();
+        }
+
     }
 }
---
+++ NhaMayThep.Application/QuaTrinhNhanSu/UpdateQuaTrinhNhanSu/UpdateQuaTrinhNhanSuCommand.cs
@@ -0,0 +1,37 @@
+﻿using MediatR;
+using NhaMayThep.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhaMayThep.Application.QuaTrinhNhanSu.UpdateQuaTrinhNhanSu
+{
+    public class UpdateQuaTrinhNhanSuCommand : IRequest, ICommand
+    {
+        public UpdateQuaTrinhNhanSuCommand(string id
+            , int loaiQuaTrinhID
+            , DateTime? ngayKetThuc
+            , int phongBanID
+            , int chucVuID
+            , int chucDanhID
+            , string? ghiChu)
+        {
+            ID = id;
+            LoaiQuaTrinhID = loaiQuaTrinhID;
+            NgayKetThuc = ngayKetThuc;
+            PhongBanID = phongBanID;
+            ChucVuID = chucVuID;
+            ChucDanhID = chucDanhID;
+            GhiChu = ghiChu;
+        }
+        public string ID { get; set; }
+        public int LoaiQuaTrinhID { get; set; }
+        public DateTime? NgayKetThuc { get; set; }
+        public int PhongBanID { get; set; }
+        public int ChucVuID { get; set; }
+        public int ChucDanhID { get; set; }
+        public string? GhiChu { get; set; }
+    }
+}
