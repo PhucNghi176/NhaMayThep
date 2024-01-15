d535333dbbc77a7df63012ef37192f87eb44fca8
NemissU <namphse171651@fpt.edu.vn>
2024-01-15 14:46:11 +0700

    Nam:Done Delete PhongBan

---
+++ NhaMayThep.Application/PhongBan/DeletePhongBan/DeletePhongBanCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using AutoMapper;
+using MediatR;
+using NhaMapThep.Domain.Repositories.ConfigTable;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhaMayThep.Application.PhongBan.DeletePhongBan
+{
+    public class DeletePhongBanCommandHandler : IRequestHandler<DeletePhongBanCommand>
+    {
+        private readonly IMapper _mapper;
+        private readonly IPhongBanRepository _phongBanRepository;
+        public DeletePhongBanCommandHandler(IPhongBanRepository phongBanRepository, IMapper mapper)
+        {
+            _phongBanRepository = phongBanRepository;
+            _mapper = mapper;
+        }
+        public async Task Handle(DeletePhongBanCommand command, CancellationToken cancellationToken)
+        {
+            var phongBan = _phongBanRepository.FindAsync(x => x.ID == command.ID).Result;
+            _phongBanRepository.Remove(phongBan);
+            await _phongBanRepository.UnitOfWork.SaveChangesAsync(cancellationToken);
+        }
+    }
+}
