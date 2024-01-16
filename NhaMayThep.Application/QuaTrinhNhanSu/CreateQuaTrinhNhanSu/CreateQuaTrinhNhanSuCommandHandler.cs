f567129f7d78c7fddc41010cf451a392adcf656b
NemissU <namphse171651@fpt.edu.vn>
2024-01-16 15:37:22 +0700

    fixed conflict

--- NhaMayThep.Application/QuaTrinhNhanSu/CreateQuaTrinhNhanSu/CreateQuaTrinhNhanSuCommandHandler.cs
+++ NhaMayThep.Application/QuaTrinhNhanSu/CreateQuaTrinhNhanSu/CreateQuaTrinhNhanSuCommandHandler.cs
@@ -19,7 +19,7 @@ namespace NhaMayThep.Application.QuaTrinhNhanSu.CreateQuaTrinhNhanSu
         public async Task<QuaTrinhNhanSuDto> Handle(CreateQuaTrinhNhanSuCommand command, CancellationToken cancellationToken)
         {
             QuaTrinhNhanSuEntity entity = new QuaTrinhNhanSuEntity()
-            {
+    {
                 LoaiQuaTrinhID = command.LoaiQuaTrinhID,
                 MaSoNhanVien = command.MaSoNhanVien,
                 NgayBatDau = command.NgayBatDau,
