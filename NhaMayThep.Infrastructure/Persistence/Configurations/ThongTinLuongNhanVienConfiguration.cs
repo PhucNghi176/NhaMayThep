4dc1cd89e6854fe9ce21f5f838159dd3c8b67a0e
PhucNghi176 <phamphucnghi1706@gmail.com>
2024-01-16 11:57:15 +0700

    update entity ChinhSachNhanSu + ThongTinLuongNhanVIen

---
+++ NhaMayThep.Infrastructure/Persistence/Configurations/ThongTinLuongNhanVienConfiguration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using NhaMapThep.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhaMayThep.Infrastructure.Persistence.Configurations
+{
+    public class ThongTinLuongNhanVienConfiguration : IEntityTypeConfiguration<ThongTinLuongNhanVienEntity>
+    {
+        public void Configure(EntityTypeBuilder<ThongTinLuongNhanVienEntity> builder)
+        {
+            
+
+            builder.HasOne(x=>x.NhanVien)
+                .WithMany()
+                .HasForeignKey(x=>x.MaSoNhanVien)
+                .OnDelete(DeleteBehavior.Restrict);
+                
+        }
+    }
+}
