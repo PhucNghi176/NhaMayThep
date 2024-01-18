d249f2b7eae5ebb9fc75b7309b3d09db3c001b60
NemissU <namphse171651@fpt.edu.vn>
2024-01-18 08:50:17 +0700

    Nam: done ThongTinQuaTrinhNhanSu

---
+++ NhaMayThep.Application/ThongTinQuaTrinhNhanSu/CreateThongTinQuaTrinhNhanSu/CreateThongTinQuaTrinhNhanSuCommandHandler.cs
@@ -0,0 +1,40 @@
+﻿using MediatR;
+using NhaMapThep.Domain.Entities.ConfigTable;
+using NhaMapThep.Domain.Repositories.ConfigTable;
+using NhaMayThep.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhaMayThep.Application.ThongTinQuaTrinhNhanSu.CreateThongTinQuaTrinhNhanSu
+{
+    public class CreateThongTinQuaTrinhNhanSuCommandHandler : IRequestHandler<CreateThongTinQuaTrinhNhanSuCommand, string>
+    {
+        IThongTinQuaTrinhNhanSuRepository _thongTinQuaTrinhNhanSuRepository;
+        ICurrentUserService _currentUserService;
+        public CreateThongTinQuaTrinhNhanSuCommandHandler(ICurrentUserService currentUserService, IThongTinQuaTrinhNhanSuRepository thongTinQuaTrinhNhanSuRepository)
+        {
+            _currentUserService = currentUserService;
+            _thongTinQuaTrinhNhanSuRepository = thongTinQuaTrinhNhanSuRepository;
+        }
+        public async Task<string> Handle(CreateThongTinQuaTrinhNhanSuCommand command, CancellationToken cancellationToken)
+        {
+            var existName = await _thongTinQuaTrinhNhanSuRepository.AnyAsync(x => x.Name == command.Name && x.NguoiXoaID == null);
+            if(existName == true)
+            {
+                throw new DuplicateNameException("Thông tin quá trình nhân sự: " + command.Name + " đã tồn tại");
+            }
+            ThongTinQuaTrinhNhanSuEntity entity = new ThongTinQuaTrinhNhanSuEntity()
+            {
+                Name = command.Name,
+                NguoiTaoID = _currentUserService.UserId,
+                NgayTao = DateTime.UtcNow
+            };
+            _thongTinQuaTrinhNhanSuRepository.Add(entity);
+            return await _thongTinQuaTrinhNhanSuRepository.UnitOfWork.SaveChangesAsync(cancellationToken) > 0 ? "Tạo thành công" : "Tạo thất bại";
+        }
+    }
+}
---
+++ NhaMayThep.Application/ThongTinQuaTrinhNhanSu/DeleteThongTinQuaTrinhNhanSu/DeleteThongTinQuaTrinhNhanSuCommandHandler.cs
@@ -0,0 +1,35 @@
+﻿using MediatR;
+using NhaMapThep.Domain.Common.Exceptions;
+using NhaMapThep.Domain.Repositories.ConfigTable;
+using NhaMayThep.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhaMayThep.Application.ThongTinQuaTrinhNhanSu.DeleteThongTinQuaTrinhNhanSu
+{
+    public class DeleteThongTinQuaTrinhNhanSuCommandHandler : IRequestHandler<DeleteThongTinQuaTrinhNhanSuCommand, string>
+    {
+        IThongTinQuaTrinhNhanSuRepository _thongTinQuaTrinhNhanSuRepository;
+        ICurrentUserService _currentUserService;
+        public DeleteThongTinQuaTrinhNhanSuCommandHandler(ICurrentUserService currentUserService, IThongTinQuaTrinhNhanSuRepository thongTinQuaTrinhNhanSuRepository)
+        {
+            _currentUserService = currentUserService;
+            _thongTinQuaTrinhNhanSuRepository = thongTinQuaTrinhNhanSuRepository;
+        }
+        public async Task<string> Handle(DeleteThongTinQuaTrinhNhanSuCommand command, CancellationToken cancellationToken)
+        {
+            var entity = await _thongTinQuaTrinhNhanSuRepository.FindAsync(x => x.ID == command.ID && x.NguoiXoaID == null);
+            if (entity == null)
+            {
+                throw new NotFoundException("Thông tin quá trình nhân sự không tồn tại");
+            }
+            entity.NgayXoa = DateTime.UtcNow;
+            entity.NguoiXoaID = _currentUserService.UserId;
+            _thongTinQuaTrinhNhanSuRepository.Update(entity);
+            return await _thongTinQuaTrinhNhanSuRepository.UnitOfWork.SaveChangesAsync(cancellationToken) > 0 ? "Xóa thành công" : "Xóa thất bại";
+        }
+    }
+}
