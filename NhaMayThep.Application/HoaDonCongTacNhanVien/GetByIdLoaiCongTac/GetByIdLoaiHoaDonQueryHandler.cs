0a5116a0c3ad61f8924b4eadd22a180dc8dfe3c2
tententamy <minhtampro4545@gmail.com>
2024-01-26 22:57:53 +0700

    finish search by year and month

---
+++ NhaMayThep.Application/HoaDonCongTacNhanVien/GetByIdLoaiCongTac/GetByIdLoaiHoaDonQueryHandler.cs
@@ -0,0 +1,47 @@
+﻿using AutoMapper;
+using MediatR;
+using NhaMapThep.Domain.Common.Exceptions;
+using NhaMapThep.Domain.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhaMayThep.Application.HoaDonCongTacNhanVien.GetByIdLoaiCongTac
+{
+    public class GetByIdLoaiHoaDonQueryHandler : IRequestHandler<GetByIdLoaiHoaDonQuery, List<HoaDonCongTacNhanVienDto>>
+    {
+        private readonly IHoaDonCongTacNhanVienRepository _hoaDonCongTacNhanVienRepository;
+        private readonly IMapper _mapper;
+        private readonly ILoaiHoaDonRepository _loaiHoaDonRepository;
+
+        public GetByIdLoaiHoaDonQueryHandler(IHoaDonCongTacNhanVienRepository hoaDonCongTacNhanVienRepository,
+            IMapper mapper, ILoaiHoaDonRepository loaiHoaDonRepository)
+        {
+            _hoaDonCongTacNhanVienRepository = hoaDonCongTacNhanVienRepository;
+            _mapper = mapper;
+            _loaiHoaDonRepository = loaiHoaDonRepository;
+        }
+
+        public async Task<List<HoaDonCongTacNhanVienDto>> Handle(GetByIdLoaiHoaDonQuery request, CancellationToken cancellationToken)
+        {
+            var list = await _hoaDonCongTacNhanVienRepository.FindAllAsync(x =>
+             x.LoaiHoaDonID == request.idLoaiHoaDon &&
+            !x.NgayXoa.HasValue &&
+             x.DuongDanFile.Contains($"/{request.year}/{request.month}/"),
+             cancellationToken);
+
+            var loaiHoaDon = await _loaiHoaDonRepository.FindAsync(x => x.ID == request.idLoaiHoaDon,cancellationToken);
+            if(loaiHoaDon == null) 
+            {
+                throw new NotFoundException("Loại Công Tác Trên không Tồn Tại");
+            }
+            if(list == null || !list.Any())
+            {
+                throw new NotFoundException($"Không Tìm Thấy Tệp tin nào Tại Loại Hóa Đon :{loaiHoaDon.Name} vào năm :{request.year}, tháng : {request.month} ");
+            }
+            return list.MapToHoaDonCongTacNhanVienDtoList(_mapper);
+        }
+    }
+}
---
+++ NhaMayThep.Application/HoaDonCongTacNhanVien/GetByIdLoaiCongTac/GetByIdLoaiHoaDonQueryvalidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhaMayThep.Application.HoaDonCongTacNhanVien.GetByIdLoaiCongTac
+{
+    public class GetByIdLoaiHoaDonQueryvalidator : AbstractValidator<GetByIdLoaiHoaDonQuery>
+    {
+        public GetByIdLoaiHoaDonQueryvalidator()
+        {
+            RuleFor(command => command.idLoaiHoaDon)
+                .NotEmpty().WithMessage("idLoaiCongTac không được để trống.");
+
+            RuleFor(command => command.year)
+                .NotEmpty().WithMessage("năm không được để trống.")
+                .Must(year => year.Length == 4 && int.TryParse(year, out _)).WithMessage("năm bạn nhập không hợp lệ");
+
+
+            RuleFor(command => command.month)
+                .NotEmpty().WithMessage("tháng không được để trống.")
+                .Must(month => month >= 1 && month <= 12).WithMessage("tháng phải nằm trong khoảng từ 1 đến 12.");
+        }
+    }
+}
