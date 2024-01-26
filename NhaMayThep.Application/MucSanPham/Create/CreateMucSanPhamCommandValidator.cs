b115eae35cbb782faa1e3ae9c177c44449d7822f
NemissU <namphse171651@fpt.edu.vn>
2024-01-26 15:41:40 +0700

    Nam: done MucSanPham, Fixed PhongBan

---
+++ NhaMayThep.Application/MucSanPham/Create/CreateMucSanPhamCommandValidator.cs
@@ -0,0 +1,35 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhaMayThep.Application.MucSanPham.Create
+{
+    public class CreateMucSanPhamCommandValidator : AbstractValidator<CreateMucSanPhamCommand>
+    {
+        public CreateMucSanPhamCommandValidator()
+        {
+            ConfigureValidationRules();
+        }
+
+        private void ConfigureValidationRules()
+        {
+            RuleFor(v => v.LuongMucSanPham)
+                .NotNull().WithMessage("LuongMucSanPham không được trống")
+                .GreaterThan(0).WithMessage("LuongMucSanPham phải lớn hơn 0");
+
+            RuleFor(v => v.MucSanPhamToiThieu)
+                .NotNull().WithMessage("MucSanPhamToiThieu không được trống")
+                .GreaterThanOrEqualTo(0).WithMessage("MucSanPhamToiThieu phải lớn hơn hoặc bằng 0");
+
+            RuleFor(v => v.MucSanPhamToiDa)
+                .NotNull().WithMessage("MucSanPhamToiDa không được trống")
+                .GreaterThan(0).WithMessage("MucSanPhamToiDa phải lớn hơn 0");
+
+            RuleFor(v => v.Name)
+                .NotEmpty().WithMessage("Name không được trống");
+        }
+    }
+}
---
+++ NhaMayThep.Application/MucSanPham/GetById/GetMucSanPhamByIdQuery.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using NhaMayThep.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhaMayThep.Application.MucSanPham.GetById
+{
+    public class GetMucSanPhamByIdQuery : IRequest<MucSanPhamDto>, IQuery
+    {
+        public GetMucSanPhamByIdQuery(int id)
+        {
+            ID = id;
+        }
+        public int ID { get; set; }
+    }
+}
