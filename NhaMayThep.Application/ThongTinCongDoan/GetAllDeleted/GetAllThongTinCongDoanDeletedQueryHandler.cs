460c0f4e7feeeac49a26b4215386825c72aeaf3d
lesonhai0339 <haileds939@gmail.com>
2024-01-29 15:04:19 +0700

    up_mapper

--- NhaMayThep.Application/ThongTinCongDoan/GetAllDeleted/GetAllThongTinCongDoanDeletedQueryHandler.cs
+++ NhaMayThep.Application/ThongTinCongDoan/GetAllDeleted/GetAllThongTinCongDoanDeletedQueryHandler.cs
@@ -14,22 +14,27 @@ namespace NhaMayThep.Application.ThongTinCongDoan.GetAllDeleted
     {
         private readonly IThongTinCongDoanRepository _thongtinCongDoanRepository;
         private readonly IMapper _mapper;
+        private readonly INhanVienRepository _nhanVienRepository;
         public GetAllThongTinCongDoanDeletedQueryHandler(
             IThongTinCongDoanRepository thongTinCongDoanRepository,
-            IMapper mapper)
+            IMapper mapper,
+            INhanVienRepository nhanVienRepository)
         {
             _thongtinCongDoanRepository = thongTinCongDoanRepository;
             _mapper = mapper;
+            _nhanVienRepository = nhanVienRepository;
         }
         public async Task<List<ThongTinCongDoanDto>> Handle(GetAllThongTinCongDoanDeletedQuery request, CancellationToken cancellationToken)
         {
             var thongtincongdoans = await _thongtinCongDoanRepository
                 .FindAllAsync(x => x.NgayXoa.HasValue && x.NguoiXoaID != null, cancellationToken);
+            var nhanviens = await _nhanVienRepository
+                .FindAllToDictionaryAsync(x => !x.NgayXoa.HasValue && x.NguoiXoaID == null, x => x.ID, x => x.HoVaTen, cancellationToken);
             if (thongtincongdoans == null || !thongtincongdoans.Any())
             {
                 throw new NotFoundException("Không tồn tại bất kì thông tin công đoàn nào");
             }
-            return thongtincongdoans.MapToThongTinCongDoanDtoList(_mapper);
+            return thongtincongdoans.MapToThongTinCongDoanDtoList(_mapper, nhanviens);
         }
     }
 }
