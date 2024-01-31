ac9a2613b8ea0430008bf808f9dce8d73642ff08
NemissU <namphse171651@fpt.edu.vn>
2024-01-31 19:06:35 +0700

    Nam:fixed MucSanPham API, fixed name of QuaTrinhNhanSu

--- NhaMayThep.Application/MucSanPham/Update/UpdateMucSanPhamCommandHandler.cs
+++ NhaMayThep.Application/MucSanPham/Update/UpdateMucSanPhamCommandHandler.cs
@@ -28,18 +28,10 @@ namespace NhaMayThep.Application.MucSanPham.Update
             {
                 throw new NotFoundException("ID không tồn tại");
             }
-
-            var existName = await _mucSanPhamRepository.AnyAsync(x => x.Name == command.Name && x.ID != command.ID && x.NguoiXoaID == null);
-            if (existName)
-            {
-                throw new DuplicateNameException("Name của MucSanPham đã tồn tại");
-            }
-
-            existEntity.Name = command.Name;
             existEntity.LuongMucSanPham = command.LuongMucSanPham;
             existEntity.MucSanPhamToiDa = command.MucSanPhamToiDa;
             existEntity.MucSanPhamToiThieu = command.MucSanPhamToiThieu;
-            existEntity.NgayCapNhat = DateTime.UtcNow;
+            existEntity.NgayCapNhatCuoi = DateTime.UtcNow;
             existEntity.NguoiCapNhatID = _currentUserService.UserId;
             _mucSanPhamRepository.Update(existEntity);
             return await _mucSanPhamRepository.UnitOfWork.SaveChangesAsync(cancellationToken) > 0 ? "Cập nhật thành công" : "Cập nhật thất bại";
--- NhaMayThep.Infrastructure/Repositories/MucSanPhamRepository.cs
+++ NhaMayThep.Infrastructure/Repositories/MucSanPhamRepository.cs
@@ -1,6 +1,6 @@
 ﻿using AutoMapper;
 using NhaMapThep.Domain.Entities;
-using NhaMapThep.Domain.Repositories.ConfigTable;
+using NhaMapThep.Domain.Repositories;
 using NhaMapThep.Infrastructure.Repositories;
 using NhaMayThep.Infrastructure.Persistence;
 using System;
