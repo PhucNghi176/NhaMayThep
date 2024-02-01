9803ee3d0d89123c95525ce75bb6511828c3b974
PhucNghi176 <phamphucnghi1706@gmail.com>
2024-02-01 15:58:18 +0700

    Remove unnecessary lines from .gitignore file

---
+++ NhaMayThep.Infrastructure/Migrations/TriggerNPT.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhaMayThep.Infrastructure.Migrations
+{
+    public partial class TriggerNPT : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("CREATE TRIGGER tg_insert_npt\r\nON dbo.ThongTinGiamTruGiaCanh\r\nAFTER INSERT\r\nAS\r\nBEGIN\r\nUPDATE NhanVien\r\nSET SoNguoiPhuThuoc = SoNguoiPhuThuoc+1\r\nWHERE ID IN (SELECT NhanVienID FROM inserted)\r\nEND");
+
+            migrationBuilder.Sql("CREATE TRIGGER tg_update_npt\r\nON dbo.ThongTinGiamTruGiaCanh\r\nAFTER UPDATE\r\nAS  \r\nBEGIN\r\nUPDATE NhanVien\r\nSET SoNguoiPhuThuoc = SoNguoiPhuThuoc-1\r\nWHERE ID IN (SELECT NhanVienID FROM inserted)\r\nEND");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DROP TRIGGER tg_insert_npt");
+            migrationBuilder.Sql("DROP TRIGGER tg_update_npt");
+        }
+    }
+}
