c3f6a72e469bc832eb0aab0c598e0baa670e366a
ThanhIsHereee <nquangthanh88@gmail.com>
2024-02-23 18:56:20 +0700

    gộp 2 api filter bi ChucVuID & TinhTrangLamViecID

---
+++ NhaMayThep.Application/NhanVien/FillterByChucVuIDOrTinhTrangLamViecID/FillterByChucVuIDOrTinhTrangLamViecIDQuery.cs
@@ -0,0 +1,27 @@
+﻿using MediatR;
+using NhaMapThep.Application.Common.Pagination;
+using NhaMayThep.Application.Common.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhaMayThep.Application.NhanVien.FillterByChucVuIDOrTinhTrangLamViecID
+{
+    public class FillterByChucVuIDOrTinhTrangLamViecIDQuery : IRequest<PagedResult<NhanVienDto>>, IQuery
+    {
+        public int no { get; set; }
+        public int pageSize {  get; set; }
+        public int chucvuID {  get; set; }
+        public int tinhtranglamviecID {  get; set; }
+        public FillterByChucVuIDOrTinhTrangLamViecIDQuery() { }
+        public FillterByChucVuIDOrTinhTrangLamViecIDQuery(int no, int pageSize, int chucvuID, int tinhtranglamviecID)
+        {
+            this.no = no;
+            this.pageSize = pageSize;
+            this.chucvuID = chucvuID;
+            this.tinhtranglamviecID = tinhtranglamviecID;
+        }
+    }
+}
