6a4110adcd70c75a3d117505f4c9ed1b215ca508
PhucNghi176 <phamphucnghi1706@gmail.com>
2024-03-12 10:36:24 +0700

    update

--- NhaMayThep.Application/CanCuocCongDan/UpdateCanCuocCongDan/UpdateCanCuocCongDanCommandHandler.cs
+++ NhaMayThep.Application/CanCuocCongDan/UpdateCanCuocCongDan/UpdateCanCuocCongDanCommandHandler.cs
@@ -19,7 +19,7 @@ namespace NhaMayThep.Application.CanCuocCongDan.UpdateCanCuocCongDan
         public async Task<string> Handle(UpdateCanCuocCongDanCommand request, CancellationToken cancellationToken)
         {
             var CanCuocCongDan = await _canCuocCongDanRepository.FindAsync(x => x.NhanVienID == request.NhanVienID && x.NgayXoa == null, cancellationToken) ?? throw new NotFoundException("Nhân Viên Không Có Cân Cước Công Dân");
-            var IsExsisted = await _canCuocCongDanRepository.AnyAsync(x => x.CanCuocCongDan == request.CanCuocCongDan && x.NhanVienID != request.NhanVienID && x.NgayXoa == null, cancellationToken);
+            bool IsExsisted = await _canCuocCongDanRepository.AnyAsync(x => x.CanCuocCongDan == request.CanCuocCongDan && x.NhanVienID != request.NhanVienID && x.NgayXoa == null, cancellationToken);
             if (IsExsisted) { throw new DuplicationException("Căn Cước Công Dân này đã tồn tại "); }
 
             CanCuocCongDan.HoVaTen = request.HoVaTen;
