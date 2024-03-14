69a4c49c1b15dcfd2f9e073d5fe103e59c5a1a46
lesonhai0339 <haileds939@gmail.com>
2024-03-14 21:34:29 +0700

    update_Dto

--- NhaMayThep.Application/ThongTinCongDoan/ThongTinCongDoanDto.cs
+++ NhaMayThep.Application/ThongTinCongDoan/ThongTinCongDoanDto.cs
@@ -7,17 +7,19 @@ namespace NhaMayThep.Application.ThongTinCongDoan
     public class ThongTinCongDoanDto : IMapFrom<ThongTinCongDoanEntity>
     {
         public string Id { get; set; } = null!;
+        public string? NhanVienID { get; set; }
         public string NhanVien { get; set; } = null!;
         public bool ThuKiCongDoan { get; set; }
         public DateTime NgayGiaNhap { get; set; }
-        public static ThongTinCongDoanDto CreateThongTinCongDoan(string id, string nhanvien, bool thukycongdoan, DateTime ngaygianhap)
+        public static ThongTinCongDoanDto CreateThongTinCongDoan(string id, string nhanvien, bool thukycongdoan, DateTime ngaygianhap, string nhanvienid)
         {
             return new ThongTinCongDoanDto
             {
                 Id = id,
                 NhanVien = nhanvien,
                 ThuKiCongDoan = thukycongdoan,
-                NgayGiaNhap = ngaygianhap
+                NgayGiaNhap = ngaygianhap,
+                NhanVienID = nhanvienid
             };
         }
 
