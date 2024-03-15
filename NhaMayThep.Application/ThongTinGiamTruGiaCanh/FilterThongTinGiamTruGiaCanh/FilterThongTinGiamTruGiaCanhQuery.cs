8b849580be21f1ff3056f9764f8fb0cfb06b188c
lesonhai0339 <haileds939@gmail.com>
2024-03-15 14:17:02 +0700

    up

---
+++ NhaMayThep.Application/ThongTinGiamTruGiaCanh/FilterThongTinGiamTruGiaCanh/FilterThongTinGiamTruGiaCanhQuery.cs
@@ -0,0 +1,14 @@
+﻿using MediatR;
+using NhaMapThep.Application.Common.Pagination;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhaMayThep.Application.ThongTinGiamTruGiaCanh.FilterThongTinGiamTruGiaCanh
+{
+    public class FilterThongTinGiamTruGiaCanhQuery: IRequest<PagedResult<ThongTinGiamTruGiaCanhDto>>, IRequest
+    {
+    }
+}
