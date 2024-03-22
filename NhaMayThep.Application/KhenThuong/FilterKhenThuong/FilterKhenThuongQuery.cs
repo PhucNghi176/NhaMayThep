0af9235e595c11d96d40d32ce8c31c60917e893f
NemissU <namphse171651@fpt.edu.vn>
2024-03-22 17:17:37 +0700

    Nam: fixed filter KhenThuong

--- NhaMayThep.Application/KhenThuong/FilterKhenThuong/FilterKhenThuongQuery.cs
+++ NhaMayThep.Application/KhenThuong/FilterKhenThuong/FilterKhenThuongQuery.cs
@@ -17,12 +17,12 @@ namespace NhaMayThep.Application.KhenThuong.FilterKhenThuong
         public string? MaSoNhanVien { get; set; }
         public int? ChinhSachNhanSuID { get; set; } = 0;
         public string? TenDotKhenThuong { get; set; }
-        public DateTime? NgayKhenThuong { get; set; }
+        public string? NgayKhenThuong { get; set; }
         public decimal? TongThuong { get; set; } = decimal.Zero;
 
         public FilterKhenThuongQuery() { }
 
-        public FilterKhenThuongQuery(int pageNumber, int pageSize, string maSoNhanVien, int chinhSachNhanSuID, string tenDotKhenThuong, DateTime ngayKhenThuong, decimal tongThuong)
+        public FilterKhenThuongQuery(int pageNumber, int pageSize, string maSoNhanVien, int chinhSachNhanSuID, string tenDotKhenThuong, string ngayKhenThuong, decimal tongThuong)
         {
             PageNumber = pageNumber;
             PageSize = pageSize;
