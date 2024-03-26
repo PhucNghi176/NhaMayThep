52d729bff61441802783d6fcac5dde97ea2d273a
sonhohuu <hhson365@gmail.com>
2024-03-26 23:37:27 +0700

    fix bug

--- NhaMayThep.Api/Controllers/MaDangKiCaLamController.cs
+++ NhaMayThep.Api/Controllers/MaDangKiCaLamController.cs
@@ -12,87 +12,84 @@ using NhaMayThep.Application.MaDangKiCaLamViec.GetById;
 
 namespace NhaMayThep.Api.Controllers
 {
-    public class MaDangKiCaLamController
+    [ApiController]
+    [Authorize]
+    public class MaDangKiCaLamController : ControllerBase
     {
-        [ApiController]
-        [Authorize]
-        public class LoaiHoaDonController : ControllerBase
+        private readonly ISender _mediator;
+        public MaDangKiCaLamController(ISender mediator)
         {
-            private readonly ISender _mediator;
+            _mediator = mediator;
+        }
 
-            public LoaiHoaDonController(ISender mediator)
-            {
-                _mediator = mediator;
-            }
+        [HttpPost("ma-dang-ki-ca-lam")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> CreateMaDangKiCaLam(
+           [FromBody] CreateMaDangKiCaLamCommand command,
+           CancellationToken cancellationToken = default)
+        {
+            var result = await _mediator.Send(command, cancellationToken);
+            return Ok(new JsonResponse<string>(result));
+        }
 
-            [HttpPost("ma-dang-ki-ca-lam")]
-            [Produces(MediaTypeNames.Application.Json)]
-            [ProducesResponseType(StatusCodes.Status400BadRequest)]
-            [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-            [ProducesResponseType(StatusCodes.Status403Forbidden)]
-            [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
-            public async Task<ActionResult> CreateMaDangKiCaLam(
-               [FromBody] CreateMaDangKiCaLamCommand command,
-               CancellationToken cancellationToken = default)
-            {
-                var result = await _mediator.Send(command, cancellationToken);
-                return Ok(new JsonResponse<string>(result));
-            }
+        [HttpPut("ma-dang-ki-ca-lam")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> UpdateMaDangKiCaLam(
+            [FromBody] UpdateMaDangKiCaLamCommand command,
+            CancellationToken cancellationToken = default)
+        {
+            var result = await _mediator.Send(command, cancellationToken);
+            return Ok(new JsonResponse<string>(result));
+        }
 
-            [HttpPut("ma-dang-ki-ca-lam")]
-            [Produces(MediaTypeNames.Application.Json)]
-            [ProducesResponseType(StatusCodes.Status400BadRequest)]
-            [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-            [ProducesResponseType(StatusCodes.Status403Forbidden)]
-            [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
-            public async Task<ActionResult> UpdateMaDangKiCaLam(
-                [FromBody] UpdateMaDangKiCaLamCommand command,
-                CancellationToken cancellationToken = default)
-            {
-                var result = await _mediator.Send(command, cancellationToken);
-                return Ok(new JsonResponse<string>(result));
-            }
+        [HttpDelete("ma-dang-ki-ca-lam/{id}")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult> DeleteMaDangKiCaLam(
+            [FromRoute] int id,
+            CancellationToken cancellationToken = default)
+        {
+            var result = await _mediator.Send(new DeleteMaDangKiCaLamCommand(id), cancellationToken);
+            return Ok(new JsonResponse<string>(result));
+        }
 
-            [HttpDelete("ma-dang-ki-ca-lam/{id}")]
-            [Produces(MediaTypeNames.Application.Json)]
-            [ProducesResponseType(StatusCodes.Status400BadRequest)]
-            [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-            [ProducesResponseType(StatusCodes.Status403Forbidden)]
-            [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
-            public async Task<ActionResult> DeleteMaDangKiCaLam(
-                [FromRoute] int id,
-                CancellationToken cancellationToken = default)
-            {
-                var result = await _mediator.Send(new DeleteMaDangKiCaLamCommand(id), cancellationToken);
-                return Ok(new JsonResponse<string>(result));
-            }
+        [HttpGet("ma-dang-ki-ca-lam")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<List<MaDangKiCaLamViecDTO>>> GetAllMaDangKiCaLam(
+           CancellationToken cancellationToken = default)
+        {
+            var result = await _mediator.Send(new GetAllMaDangKiCaLamQuery(), cancellationToken);
+            return Ok(new JsonResponse<List<MaDangKiCaLamViecDTO>>(result));
+        }
 
-            [HttpGet("ma-dang-ki-ca-lam")]
-            [Produces(MediaTypeNames.Application.Json)]
-            [ProducesResponseType(StatusCodes.Status400BadRequest)]
-            [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-            [ProducesResponseType(StatusCodes.Status403Forbidden)]
-            [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
-            public async Task<ActionResult<List<MaDangKiCaLamViecDTO>>> GetAllMaDangKiCaLam(
-               CancellationToken cancellationToken = default)
-            {
-                var result = await _mediator.Send(new GetAllMaDangKiCaLamQuery(), cancellationToken);
-                return Ok(new JsonResponse<List<MaDangKiCaLamViecDTO>>(result));
-            }
-
-            [HttpGet("ma-dang-ki-ca-lam/{id}")]
-            [Produces(MediaTypeNames.Application.Json)]
-            [ProducesResponseType(StatusCodes.Status400BadRequest)]
-            [ProducesResponseType(StatusCodes.Status401Unauthorized)]
-            [ProducesResponseType(StatusCodes.Status403Forbidden)]
-            [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
-            public async Task<ActionResult<MaDangKiCaLamViecDTO>> GetMaDangKiCaLamById(
-                [FromRoute] int id,
-                CancellationToken cancellationToken = default)
-            {
-                var result = await _mediator.Send(new GetMaDangKiCaLamByIdQuery(id), cancellationToken);
-                return Ok(new JsonResponse<MaDangKiCaLamViecDTO>(result));
-            }
+        [HttpGet("ma-dang-ki-ca-lam/{id}")]
+        [Produces(MediaTypeNames.Application.Json)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status401Unauthorized)]
+        [ProducesResponseType(StatusCodes.Status403Forbidden)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status500InternalServerError)]
+        public async Task<ActionResult<MaDangKiCaLamViecDTO>> GetMaDangKiCaLamById(
+            [FromRoute] int id,
+            CancellationToken cancellationToken = default)
+        {
+            var result = await _mediator.Send(new GetMaDangKiCaLamByIdQuery(id), cancellationToken);
+            return Ok(new JsonResponse<MaDangKiCaLamViecDTO>(result));
         }
     }
+
 }
