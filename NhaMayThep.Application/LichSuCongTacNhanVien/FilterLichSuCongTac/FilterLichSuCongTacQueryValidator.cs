f46037fe5f13c2c04a917b60e19052491d54414e
lama-try-code <dotranlam2003@gmail.com>
2024-03-29 13:44:25 +0700

    Add: Filter for LichSuCongTacNhanVien

---
+++ NhaMayThep.Application/LichSuCongTacNhanVien/FilterLichSuCongTac/FilterLichSuCongTacQueryValidator.cs
@@ -0,0 +1,28 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhaMayThep.Application.LichSuCongTacNhanVien.FilterLichSuCongTac
+{
+    public class FilterLichSuCongTacQueryValidator : AbstractValidator<FilterLichSuCongTacQuery>
+    {
+        public FilterLichSuCongTacQueryValidator()
+        {
+            Configure();
+        }
+
+        public void Configure()
+        {
+            RuleFor(x => x.PageNumber)
+                .NotEmpty().WithMessage("Số trang không được trống")
+                .NotNull().WithMessage("Số trang không được rỗng");
+
+            RuleFor(x => x.PageSize)
+                .NotEmpty().WithMessage("Số item trong trang không được để trống")
+                .NotNull().WithMessage("Số item trong trang không được rỗng");
+        }
+    }
+}
