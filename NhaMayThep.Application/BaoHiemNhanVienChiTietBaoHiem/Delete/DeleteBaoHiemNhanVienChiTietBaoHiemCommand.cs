cd248b48da31f32c10a98f42a96b462a6e277f68
lesonhai0339 <haileds939@gmail.com>
2024-04-01 01:17:31 +0700

    up

---
+++ NhaMayThep.Application/BaoHiemNhanVienChiTietBaoHiem/Delete/DeleteBaoHiemNhanVienChiTietBaoHiemCommand.cs
@@ -0,0 +1,21 @@
+﻿using MediatR;
+using Org.BouncyCastle.Bcpg;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NhaMayThep.Application.BaoHiemNhanVienChiTietBaoHiem.Delete
+{
+    public class DeleteBaoHiemNhanVienChiTietBaoHiemCommand: IRequest<string>
+    {
+        public string MaCTBH {  get; set; } 
+        public int MaBHNV { get; set; }
+        public DeleteBaoHiemNhanVienChiTietBaoHiemCommand(string maCTBH, int maBHNV)
+        {
+            MaCTBH = maCTBH;
+            MaBHNV = maBHNV;
+        }
+    }
+}
